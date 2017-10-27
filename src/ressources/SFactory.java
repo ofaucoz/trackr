97e7fbb290d5c000c346f275ffbe5214fd2323ab
ofaucoz <orphee.faucoz@gmail.com>
2017-10-27 12:39:14 +0200

    initial commit

---
+++ src/ressources/SFactory.java
@@ -0,0 +1,19 @@
+package ressources;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class SFactory {
+	public static Session getSession(){
+		SessionFactory factory = null;
+		try{
+	         factory = new Configuration().configure().buildSessionFactory();
+		}catch (Throwable ex) { 
+			System.err.println("Failed to create sessionFactory object." + ex);
+			throw new ExceptionInInitializerError(ex); 
+		}
+		return factory.openSession();
+		
+	}
+}
