7a8790cfe0440afddfeeb6523ec91bf71a0fec58
ofaucoz <orphee.faucoz@gmail.com>
2017-10-29 17:37:14 +0100

    retrieving tweets basic

---
+++ src/ressources/TwitterBuilder.java
@@ -0,0 +1,87 @@
+package ressources;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.Properties;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONTokener;
+
+import oauth.signpost.OAuthConsumer;
+import oauth.signpost.commonshttp.CommonsHttpOAuthConsumer;
+import oauth.signpost.exception.OAuthCommunicationException;
+import oauth.signpost.exception.OAuthExpectationFailedException;
+import oauth.signpost.exception.OAuthMessageSignerException;
+
+public class TwitterBuilder {
+
+	public TwitterBuilder() throws IOException {
+		super();
+		Properties prop = new Properties();
+		InputStream input = null;
+		input = new FileInputStream("config.properties");
+
+		// load a properties file
+		prop.load(input);
+
+		// get the property values
+		consumerKey = prop.getProperty("consumerKey");
+		consumerSecret = prop.getProperty("consumerSecret");
+		accessToken = prop.getProperty("accessToken");
+		accessTokenSecret = prop.getProperty("accessTokenSecret");
+	}
+
+	private String consumerKey;
+	private String consumerSecret;
+	private String accessToken;
+	private String accessTokenSecret;
+
+	private static final String HMAC_SHA1 = "HmacSHA1";
+
+	private static final String ENC = "UTF-8";
+
+	
+	public void request() throws UnsupportedOperationException, IOException, OAuthMessageSignerException,
+			OAuthExpectationFailedException, OAuthCommunicationException, JSONException {
+		OAuthConsumer oAuthConsumer = new CommonsHttpOAuthConsumer(consumerKey, consumerSecret);
+		oAuthConsumer.setTokenWithSecret(accessToken, accessTokenSecret);
+
+		HttpGet httpGet = new HttpGet("https://api.twitter.com/1.1/statuses/home_timeline.json");
+
+		oAuthConsumer.sign(httpGet);
+
+		HttpClient httpClient = HttpClientBuilder.create().build();
+		HttpResponse httpResponse = httpClient.execute(httpGet);
+
+		int statusCode = httpResponse.getStatusLine().getStatusCode();
+		BufferedReader reader = new BufferedReader(
+				new InputStreamReader(httpResponse.getEntity().getContent(), "UTF-8"));
+		StringBuilder builder = new StringBuilder();
+		for (String line = null; (line = reader.readLine()) != null;) {
+			System.out.println(line);
+			builder.append(line).append("\n");
+		}
+		JSONTokener tokener = new JSONTokener(builder.toString());
+		JSONArray finalResult = new JSONArray(tokener);
+
+	}
+
+	public static void main(String[] args) {
+		try {
+			TwitterBuilder twitterBuilder = new TwitterBuilder();
+			twitterBuilder.request();
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+	}
+
+}
